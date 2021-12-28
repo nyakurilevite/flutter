1301d2e937b80fe41aaaa8d3cc69e92e638ee09a
Nyakuri Levite <levite@techaffinity.com>
2021-12-28 09:32:00 +0200

    Add new changes

--- lib/services/connection_utils.dart
+++ lib/services/connection_utils.dart
@@ -1,28 +1,11 @@
 import 'dart:async';
-import 'package:connectivity/connectivity.dart';
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 
 class ConnectionUtil {
 
   static Future<bool> hasInternetInternetConnection() async {
-    bool hasConnection = false;
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    //Check if device is just connect with mobile network or wifi
-    if (connectivityResult == ConnectivityResult.mobile ||
-        connectivityResult == ConnectivityResult.wifi) {
-      //Check there is actual internet connection with a mobile network or wifi
-      if (await DataConnectionChecker().hasConnection) {
-        // Network data detected & internet connection confirmed.
-        hasConnection = true;
-      } else {
-        // Network data detected but no internet connection found.
-        hasConnection = false;
-      }
-    }
-    // device has no mobile network and wifi connection at all
-    else {
-      hasConnection = false;
-    }
-    return hasConnection;
+    bool result = await InternetConnectionChecker().hasConnection;
+    return result;
   }
 }
